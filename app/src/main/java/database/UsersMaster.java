e48a07c3b0083406095ba405233e167c88302694
Nuwana Thilakasiri <it18113082@my.sliit.lk>
2021-10-03 00:16:32 +0530

    Academic_class added with DB configurations

--- app/src/main/java/database/UsersMaster.java
+++ app/src/main/java/database/UsersMaster.java
@@ -20,4 +20,10 @@ public final class UsersMaster {
         public static final String SUBJECT = "subject";
         public static final String MESSAGE = "message";
     }
+    public static class Teacher implements BaseColumns{
+        public static final String TABLE_NAME = "teacher";
+        public static final String NAME = "name";
+        public static final String PASSWORD = "password";
+        public static final String TYPE = "type";
+    }
 }
